ce81762e8ee63f3670ee694d70d7a98fd4f48ca5
Michal Bernhard <michal@bernhard.cz>
2011-02-27 18:37:55 +0100

    file rename

--- src/main/java/cz/bernhard/playground/runhang.java
+++
@@ -1,9 +0,0 @@
-package cz.bernhard.playground;
-
-class runhang {
-	public static void main(String[] args) {
-		System.out.println("Test:");
-		double d = Double.parseDouble("2.2250738585072012e-308");
-		System.out.println("Value: " + d);
-	}
-}
