e85d8daefad39737ec888f8b98bfa148d2e7243e
Michal Bernhard <michal@bernhard.cz>
2011-06-29 00:57:26 +0200

    try/finally puzzle

---
+++ src/main/java/cz/bernhard/playground/TryFinally.java
@@ -0,0 +1,11 @@
+package cz.bernhard.playground;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: michal
+ * Date: 6/29/11
+ * Time: 12:00 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TryFinally {
+}
