aeb7b032c0936a2a7e1337c9ca2aadefa69fc801
Michal Bernhard <michal@bernhard.cz>
2011-06-29 08:26:40 +0200

    try/finally puzzler

--- src/main/java/cz/bernhard/playground/TryFinally.java
+++ src/main/java/cz/bernhard/playground/TryFinally.java
@@ -1,11 +1,23 @@
 package cz.bernhard.playground;
 
-/**
- * Created by IntelliJ IDEA.
- * User: michal
- * Date: 6/29/11
- * Time: 12:00 AM
- * To change this template use File | Settings | File Templates.
- */
+
 public class TryFinally {
+
+    public static void main(String[] args) {
+
+        System.out.println(returnBoolean());
+
+    }
+
+    private static boolean returnBoolean() {
+
+        // yep try/finally is valid java construction
+        try {
+            return true; // is returned true...
+        }
+        finally {
+            return false;  // ... or false ?  <----- THE B IS CORRECT THIS METHOD RETURNS FALSE
+        }
+    }
+
 }
